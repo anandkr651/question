4a6e564c6d0dab62ae679c0aa3960e43318ec6ce
Anand Kumar <kumaranandstudent@gmail.com>
2024-12-12 10:07:07 +0530

    questin

--- question/l21t54_spiral_matrix.cpp
+++ question/l21t54_spiral_matrix.cpp
@@ -20,25 +20,25 @@ public:
 
         while (count < total)
         {
-            for (int index = startcol; index <= endcol; index++)
+            for (int index = startcol; index <= endcol && count < total; index++)
             {
                 ans.push_back(matrix[startrow][index]);
                 count++;
             }
             startrow++;
-            for (int index = startrow; index <= endcol; index++)
+            for (int index = startrow; index <= endrow && count < total; index++)
             {
                 ans.push_back(matrix[index][endcol]);
                 count++;
             }
             endcol--;
-            for (int index = endcol; index >= startcol; index--)
+            for (int index = endcol; index >= startcol && count < total; index--)
             {
                 ans.push_back(matrix[endrow][index]);
                 count++;
             }
             endrow--;
-            for (int index = endrow; index >= startrow; index--)
+            for (int index = endrow; index >= startrow && count < total; index--)
             {
                 ans.push_back(matrix[index][startcol]);
                 count++;
@@ -50,12 +50,12 @@ public:
 };
 int main()
 {
-    vector<vector<int>> s(3, vector<int>(3));
+    vector<vector<int>> s(3, vector<int>(6));
     int p;
     cout << "enter the number";
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < 6; j++)
         {
             cin >> s[i][j];
         }
@@ -63,7 +63,7 @@ int main()
     cout << "display the data " << endl;
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < 6; j++)
         {
             cout << s[i][j] << " ";
         }
@@ -91,3 +91,45 @@ display the data
 7 8 9
 1 2 3 6 9 8 7 4 5
 PS C:\Users\DELL\OneDrive\Desktop\question\question>*/
+/*enter the number1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+display the data 
+1 2 3 4
+5 6 7 8
+9 10 11 12
+1 2 3 4 8 12 11 10 9 5 6 7 6
+PS D:\question\question>*/
+/*enter the number1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+display the data 
+1 2 3 4 5 6
+7 8 9 10 11 12
+13 14 15 16 17 18
+1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
+PS D:\question\question>*/
